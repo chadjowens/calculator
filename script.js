2bcc778695ae3c6845be6018e0f04b552d79f50a
chadjowens <chadjowens@gmail.com>
2023-12-14 15:06:30 -0600

    Added Handle Operator.

--- script.js
+++ script.js
@@ -67,3 +67,25 @@ const inputDecimal = (dot) => {
         calculator.displayValue += dot;
     }
 };
+
+// Handle Operators
+const handleOperator = (nextOperator) => {
+    const { firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.waitingForSecondOperand) {
+        calculator.operator = nextOperator;
+        return;
+    }
+
+    if (firstOperand === null) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const result = calculate(firstOperand, inputValue, operator);
+        calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+        calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+};
